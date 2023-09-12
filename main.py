041f6b17126a3f906f6617d461447d71ef723d07
Selectel Git User <imperial.blessed@gmail.com>
2023-09-12 19:42:13 +0300

    func ret

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
 from typing import Union
 
-def main() -> Union[bool, int]:
+def main(resp: bool = None) -> Union[bool, int]:
     
     a = random.randint(0, 100)
     b = random.randint(100, 142)
     c = (a+b)//(a**(b-a**(b*a)))
+
+    if resp:
+        return c
+    return False
